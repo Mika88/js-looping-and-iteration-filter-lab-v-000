c0def9d90017607498a64e0ee92d56d62195c0c8
Mika88 <mikaspivak@gmail.com>
2019-03-26 18:34:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ function findMatching(array, name) {
   const names = array.filter( function (driverName) { return driverName.toLowerCase() === name.toLowerCase();});
   return names
 }
+
+function fuzzyMatch(array, begLetters) {
+  const names = array.filter( function (driverName) { return driverName.startsWith(begLetters)});
+  return names
+}
