97cfe171b31dcf332a9163497b3beae4536bc74c
Mika88 <mikaspivak@gmail.com>
2019-03-26 18:14:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 // Code your solution in this file
-function findMatching(drivers, name) {
-  drivers.filter( function (driverName) { return driverName === name;});
+function findMatching(array, name) {
+  array.filter( function (driverName) { return driverName === name;});
 }
