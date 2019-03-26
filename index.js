68ef186231fd846314205f5a7db702d9e88c0348
Mika88 <mikaspivak@gmail.com>
2019-03-26 18:20:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Code your solution in this file
 function findMatching(array, name) {
-  const names = array.filter( function (driverName) { return driverName === name;});
+  const names = array.filter( function (driverName) { driverName === name;});
   return names
 }
