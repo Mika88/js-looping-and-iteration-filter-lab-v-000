29d4854b6992f731893d3a625a1a7aed54717dfd
Mika88 <mikaspivak@gmail.com>
2019-03-26 18:36:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,3 +8,8 @@ function fuzzyMatch(array, begLetters) {
   const names = array.filter( function (driverName) { return driverName.startsWith(begLetters)});
   return names
 }
+
+function matchName(array, name) {
+  const names = array.filter( function (driver) { return driver.name === name});
+  return names
+}
